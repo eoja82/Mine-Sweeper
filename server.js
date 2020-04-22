854f31c81d7298d6f335ea069555d055ca399843
Erik Oja <erik_oja@yahoo.com>
2020-04-22 13:08:28 -0500

    set up node/express server

---
+++ server.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const express = require("express");
+const helmet = require("helmet");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(helmet());
+
+app.use("/public", express.static(process.cwd() + "/public"));
+
+app.route('/')
+  .get(function (req, res) {
+    res.sendFile(process.cwd() + '/views/index.html');
+  })
+
+//404 Not Found Middleware
+app.use(function(req, res, next) {
+  res.status(404)
+    .type('text')
+    .send('Not Found');
+});
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`);
+}); 
