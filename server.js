17c7e59225ce999a280b2e3184f612d2efb30563
Erik Oja <erik_oja@yahoo.com>
2020-05-04 19:19:24 -0500

    single to doulbe quotes

--- server.js
+++ server.js
@@ -9,16 +9,21 @@ app.use(helmet());
 
 app.use("/public", express.static(process.cwd() + "/public"));
 
-app.route('/')
+app.route("/")
   .get(function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
+    res.sendFile(process.cwd() + "/views/index.html");
+  })
+
+app.route("/createaccount")
+  .get(function (req, res) {
+    res.sendFile(process.cwd() + "/views/createAccount.html");
   })
 
 //404 Not Found Middleware
 app.use(function(req, res, next) {
   res.status(404)
-    .type('text')
-    .send('Not Found');
+    .type("text")
+    .send("Not Found");
 });
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`);
