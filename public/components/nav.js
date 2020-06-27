99a083cb0d2a0af3be923d3372df413455a24970
Erik Oja <erik_oja@yahoo.com>
2020-06-27 17:34:31 -0500

    update for log in/out

--- public/components/nav.js
+++ public/components/nav.js
@@ -2,26 +2,19 @@ const nav = document.querySelector(".nav");
 const navLinks = [
   {link: "Home", href: "/"}, 
   {link: "Create An Account", href: "/createaccount"},
-  {link: "Log In", href: "/"}
+  {link: "Log In", href: "/", id: "login"}
 ];
 
 navLinks.forEach( x => {
   let button = document.createElement("button");
   button.className = "navButton";
-  //if (x.href !== "none") {
-    let a = document.createElement("a");
-    a.innerText = x.link;
-    a.href = x.href;
-    a.className = "navLink";
-    let hr = document.createElement("hr");
-    a.appendChild(hr);
-    button.appendChild(a);
-  //}
-  /* if (x.href === "none") {
-    button.innerText = x.link;
-    button.id = "login";
-    let hr = document.createElement("hr");
-    button.appendChild(hr);
-  } */
+  if (x.id) button.id = x.id;
+  let a = document.createElement("a");
+  a.innerText = x.link;
+  a.href = x.href;
+  a.className = "navLink";
+  let hr = document.createElement("hr");
+  a.appendChild(hr);
+  button.appendChild(a);
   nav.appendChild(button);
 });
