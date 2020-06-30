2212c38fbb59c98f0426f1e8a91331c4085dafc2
Erik Oja <erik_oja@yahoo.com>
2020-06-29 20:41:34 -0500

    fix login modal flashing twice

--- public/components/nav.js
+++ public/components/nav.js
@@ -2,17 +2,17 @@ const nav = document.querySelector(".nav");
 const navLinks = [
   {link: "Home", href: "/"}, 
   {link: "Create An Account", href: "/createaccount"},
-  {link: "Log In", href: "/", id: "login"}
+  {link: "Log In", href: "javascript:void(0)", id: "login"}
 ];
 
 navLinks.forEach( x => {
   let button = document.createElement("button");
   button.className = "navButton";
-  if (x.id) button.id = x.id;
   let a = document.createElement("a");
   a.innerText = x.link;
   a.href = x.href;
   a.className = "navLink";
+  if (x.id) a.id = x.id;
   let hr = document.createElement("hr");
   a.appendChild(hr);
   button.appendChild(a);
