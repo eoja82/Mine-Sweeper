2a32668c4f492dfd22c101d9f259ca6667921bbf
Erik Oja <erik_oja@yahoo.com>
2021-03-11 17:21:39 -0600

    move nav hover underline to css

--- public/nav.js
+++ public/nav.js
@@ -12,22 +12,3 @@ function displayNav() {
   }
   else nav.className = "nav"
 }
-
-// underline nav links on mouseenter
-navLinks.forEach( x => {
-  x.addEventListener("mouseenter", underlineNavLink)
-})
-
-function underlineNavLink(event) {
-  // disable if touched not mouseenter
-  if (event.webkitForce > 0 || event.mozPressure > 0) return;
-  event.target.firstElementChild.style.visibility = "visible";
-  event.target.firstElementChild.style.width = "100%";
-
-  event.target.addEventListener("mouseleave", mouseOut);
-
-  function mouseOut(event) {
-    event.target.firstElementChild.style.visibility = "hidden";
-    event.target.firstElementChild.style.width = "0";
-  }
-}
